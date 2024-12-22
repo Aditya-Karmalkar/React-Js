182e2dfead699f14478d677433ba9bff543324fe
Aditya-Karmalkar <126249135+Aditya-Karmalkar@users.noreply.github.com>
2024-12-22 06:28:08 +0000

    commit

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,9 +2,14 @@
 import Chai from "./chai"
 
 function App() {
+  const username = "Aditya Karmalkar"
+
   return (
     // <h1>React with Vite | Aditya</h1>
+    <>
     <Chai />
+    <h2>Ak as {username}</h2>
+    </>
   )
 }
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,22 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App ! | AK</h1>
+    </div>
+  )
+} 
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click me to visit google'
+)
+
+createRoot(document.getElementById('root')).
+render( 
+    <App/>
 )
